3562ebcd4f59ffd6cf2e453f6beb47d81f672a27
Isa1006 <88344150+Isa1006@users.noreply.github.com>
2023-06-15 11:27:26 -0300

    Delete users.js

--- users.js
+++
@@ -1,5 +0,0 @@
-let users = new Map();//vamos a usar un objeto map que permite la almacenar "claves-valor" donde cada clave es unica y es clave para este caso particular
-users.set("valentino", {username: "valentino", password: "russo"});
-users.set("isabel", {username: "isabel", password: "delacre"});//utilizamos el metodo "set()"para dejar en claro que cada elemento ("dibu", "lio") y un valor con un "user" y una "password" 
- 
-export default users;//este export permite que el "map" y "Users" sea utilizados en otros archivos de javascript
